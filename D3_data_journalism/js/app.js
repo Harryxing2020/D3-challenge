d7b116f8c3cf13a40359ae18c26eed935c8badb5
harryxing@hotmail.com <harryxing@hotmail.com>
2020-09-27 13:57:37 -0700

    the 5rh commit

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -2,10 +2,10 @@ var svgWidth = 960;
 var svgHeight = 500;
 
 var margin = {
-  top: 20,
-  right: 40,
-  bottom: 60,
-  left: 100
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 100
 };
 
 var width = svgWidth - margin.left - margin.right;
@@ -13,19 +13,19 @@ var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
 var svg = d3.select(".chart")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
 var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("../data/data.csv").then(function(censusData) {
+d3.csv("../data/data.csv").then(function (censusData) {
 
     // Step 1: Parse Data/Cast as numbers
     // ==============================
-    censusData.forEach(function(data) {
+    censusData.forEach(function (data) {
         data.id = +data.id;
         data.age = +data.age;
         data.ageMoe = +data.ageMoe;
@@ -50,12 +50,12 @@ d3.csv("../data/data.csv").then(function(censusData) {
     // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([8,  d3.max(censusData, d => d.poverty)+2])
-      .range([0, width]);
+        .domain([8, d3.max(censusData, d => d.poverty) + 2])
+        .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([4, d3.max(censusData, d => d.healthcare)+2])
-      .range([height, 0]);
+        .domain([4, d3.max(censusData, d => d.healthcare) + 2])
+        .range([height, 0]);
 
     // Step 3: Create axis functions
     // ==============================
@@ -65,28 +65,58 @@ d3.csv("../data/data.csv").then(function(censusData) {
     // Step 4: Append Axes to the chart
     // ==============================
     chartGroup.append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(bottomAxis);
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
 
     chartGroup.append("g")
-      .call(leftAxis);
-    console.log("-----------------------------")
-    console.log(d3.max(censusData, d => d.healthcare))
-    console.log(d3.max(censusData, d => d.poverty))
+        .call(leftAxis);
+
+    // Step 5: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(censusData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", "15")
+        .attr("fill", "SteelBlue")
+        .attr("opacity", ".5");
+
+
+        var circleLabels = chartGroup.selectAll(null).data(censusData).enter().append("text");
+
+        circleLabels
+          .attr("x", function(d) {
+            return xLinearScale(d.poverty);
+          })
+          .attr("y", function(d) {
+            return yLinearScale(d.healthcare);
+          })
+          .text(function(d) {
+            return d.abbr;
+          })
+          .attr("font-family", "sans-serif")
+          .attr("font-size", "10px")
+          .attr("text-anchor", "middle")
+          .attr("fill", "white");
+        
+        // Create axes labels
+        chartGroup.append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 0 - margin.left + 40)
+          .attr("x", 0 - (height / 2))
+          .attr("dy", "1em")
+          .attr("class", "axisText")
+          .text("Lacks Healthcare (%)");
+        
+        chartGroup.append("text")
+          .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+          .attr("class", "axisText")
+          .text("In Poverty (%)");
 
 
 
-    // // Step 5: Create Circles
-    // // ==============================
-    // var circlesGroup = chartGroup.selectAll("circle")
-    // .data(hairData)
-    // .enter()
-    // .append("circle")
-    // .attr("cx", d => xLinearScale(d.hair_length))
-    // .attr("cy", d => yLinearScale(d.num_hits))
-    // .attr("r", "15")
-    // .attr("fill", "pink")
-    // .attr("opacity", ".5");
 
     // // Step 6: Initialize tool tip
     // // ==============================
@@ -124,6 +154,6 @@ d3.csv("../data/data.csv").then(function(censusData) {
     //   .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
     //   .attr("class", "axisText")
     //   .text("Hair Metal Band Hair Length (inches)");
-  }).catch(function(error) {
+}).catch(function (error) {
     console.log(error);
-  });
+});
