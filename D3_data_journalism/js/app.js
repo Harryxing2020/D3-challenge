7be189a821e9525c38974420df41529c992b2756
harryxing@hotmail.com <harryxing@hotmail.com>
2020-09-27 14:22:40 -0700

    the 6th commit

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -72,50 +72,52 @@ d3.csv("../data/data.csv").then(function (censusData) {
         .call(leftAxis);
 
     // Step 5: Create Circles
-    // ==============================
+    // append circles to data points
     var circlesGroup = chartGroup.selectAll("circle")
         .data(censusData)
         .enter()
         .append("circle")
-        .attr("cx", d => xLinearScale(d.poverty))
-        .attr("cy", d => yLinearScale(d.healthcare))
-        .attr("r", "15")
+        .attr("r", "11")
         .attr("fill", "SteelBlue")
         .attr("opacity", ".5");
 
+    var circleLabels = chartGroup.selectAll(null).data(censusData).enter().append("text");
 
-        var circleLabels = chartGroup.selectAll(null).data(censusData).enter().append("text");
-
-        circleLabels
-          .attr("x", function(d) {
+    circleLabels
+        .attr("x", function (d) {
             return xLinearScale(d.poverty);
-          })
-          .attr("y", function(d) {
+        })
+        .attr("y", function (d) {
             return yLinearScale(d.healthcare);
-          })
-          .text(function(d) {
+        })
+        .text(function (d) {
             return d.abbr;
-          })
-          .attr("font-family", "sans-serif")
-          .attr("font-size", "10px")
-          .attr("text-anchor", "middle")
-          .attr("fill", "white");
-        
-        // Create axes labels
-        chartGroup.append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 0 - margin.left + 40)
-          .attr("x", 0 - (height / 2))
-          .attr("dy", "1em")
-          .attr("class", "axisText")
-          .text("Lacks Healthcare (%)");
-        
-        chartGroup.append("text")
-          .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-          .attr("class", "axisText")
-          .text("In Poverty (%)");
+        })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "10px")
+        .attr("text-anchor", "middle")
+        .attr("fill", "white");
 
+    // Create axes labels
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Lacks Healthcare (%)");
 
+    chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+        .attr("class", "axisText")
+        .text("In Poverty (%)");
+
+    // ==============================
+    circlesGroup = chartGroup.selectAll("circle")
+    .transition()
+    .duration(1000)
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare));
 
 
     // // Step 6: Initialize tool tip
